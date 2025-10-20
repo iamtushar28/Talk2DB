baadaf3ef97b95bb1aaaa13c8111738420803d61
iamtushar28 <tusharsuryawanshi2232@gmail.com>
2025-10-20 20:38:33 +0530

    ui updated

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import Hero from "./components/common/Hero";
-import Result from "./components/common/Result";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Result />
     </>
   );
 }
